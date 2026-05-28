998f4be92156beb1ca45e5e5e01c5283c0b94216
Damien Crier <damien.crier@foodles.co>
2026-05-28 16:53:41 +0200

    fix(product_contract): fix same label

--- product_contract/wizards/product_contract_configurator.py
+++ product_contract/wizards/product_contract_configurator.py
@@ -24,7 +24,9 @@ class ProductContractConfigurator(models.TransientModel):
         string="Invoice Every",
         help="Invoice every (Days/Week/Month/Year)",
     )
-    recurring_rule_type = fields.Selection(related="product_id.recurring_rule_type")
+    recurring_rule_type = fields.Selection(
+        related="product_id.recurring_rule_type", string="Invoice Every type"
+    )
     recurring_invoicing_type = fields.Selection(
         related="product_id.recurring_invoicing_type"
     )
